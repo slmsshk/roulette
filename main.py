a4972f0f1ca89cd368364804c348bd57e46969e2
Salem Shaikh <slmsshk21@gmail.com>
2024-02-14 20:26:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,16 +9,21 @@ features = model.feature_names_in_
 text = st.text_input('Enter the numbers seperated with , eg:1,2,3,4,5,.....')
 
 
+if bool(text):
+    d = {k:v for k,v in zip(features,text.split(','))}
+    
+    pred = pd.DataFrame(d,index=['input']).astype(float)
+    st.write(pred)
 
-d = {k:v for k,v in zip(features,text.split(','))}
-pred = pd.DataFrame(d,index=['input']).astype(float)
+    st.markdown(f"""<h1> Model Prediction
+                """,unsafe_allow_html=True)
 
-st.markdown(f"""<h1> Model Prediction
-            """,unsafe_allow_html=True)
+    # st.write(pred)
+    predicted = model.predict(pred)
 
-predicted = model.predict(pred)
-
-if predicted ==1:
-    st.markdown(f"""<h1 style=color:red>Cheater</h1>🦹‍♀️🦹‍♂️""",unsafe_allow_html=True)
+    if predicted ==1:
+        st.markdown(f"""<h1 style=color:red>Cheater</h1>🦹‍♀️🦹‍♂️""",unsafe_allow_html=True)
+    else:
+        st.markdown(f"""<h1 style=color:red>Innocent</h1>😇""",unsafe_allow_html=True)
 else:
-    st.markdown(f"""<h1 style=color:red>Innocent</h1>😇🥺""",unsafe_allow_html=True)
+    st.write('enter text')
